aa59ac336c1fe9adeee3cc08fe4c5bf9c2484cde
LuizAngelHdz <luismx1123@gmail.com>
2024-01-25 22:44:53 -0600

    feature: creacion de la documentacion de los endpoints

--- src/app.js
+++ src/app.js
@@ -9,86 +9,82 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
 const DB_HOST = process.env.DB_HOST || "127.0.0.3";
+
 const DB_USER = process.env.DB_USER || "root";
+
 const DB_PASSWORD = process.env.DB_PASSWORD || "";
+
 const DB_NAME = process.env.DB_NAME || "blogexam";
+
 const DB_PORT = process.env.DB_PORT || 3306;
 
+/* The code `var connection = mysql.createPool({ ... })` is creating a connection pool to a MySQL
+database. */
 var connection = mysql.createPool({
-  host: DB_HOST, // si la base de datos esta en un server diferente seria la direccion o IP en la que se encuentra
-  user: DB_USER, // usuario de la base de datos
-  password: DB_PASSWORD, // contraseña de la base de datos
-  database: DB_NAME, // nombre de la base de datos
+  host: DB_HOST,
+  user: DB_USER,
+  password: DB_PASSWORD,
+  database: DB_NAME,
   port: DB_PORT,
 });
 
-// connection.connect(function (error) {
-//   if (error) {
-//     console.log(error); // Si hay un error al conectarse a la base de datos, lo mostramos en la consola
-//   } else {
-//     console.log("Conexion correcta a la base de datos"); // Si no hay errores, mostramos un mensaje de que la conexion fue correcta
-//   }
-// });
-
+/* The code `app.get("/blogs", function (req, res) { ... })` is defining a route for the GET request to
+"/blogs". When this route is accessed, it executes the callback function. */
 app.get("/blogs", function (req, res) {
-  // Consultamos todos los blogs de la base de datos
   connection.query("SELECT * FROM articles", function (error, result) {
     if (error) {
-      res.send(error); // Si hay un error en la consulta, lo enviamos como respuesta
+      res.send(error);
     } else {
-      res.send(result); // Si la consulta fue exitosa, enviamos todos los blogs como respuesta
+      res.send(result);
     }
   });
 });
 
-// Endpoint para obtener un blog
+/* The code `app.get("/blogs/:id", function (req, res) { ... })` is defining a route for the GET
+request to "/blogs/:id". This route expects a parameter `id` in the URL. When this route is
+accessed, it executes the callback function. */
 app.get("/blogs/:id", function (req, res) {
-  // Obtenemos el id del blog de la URL
   var id = req.params.id;
 
-  // Consultamos el blog con el id especificado
   connection.query(
     `SELECT * FROM articles WHERE id=${id}`,
     function (error, result) {
       if (error) {
-        res.send(error); // Si hay un error en la consulta, lo enviamos como respuesta
+        res.send(error);
       } else {
-        res.send(result); // Si la consulta fue exitosa, enviamos el blog como respuesta
+        res.send(result);
       }
     }
   );
 });
 
-// Endpoint para crear un nuevo blog
+/* The code `app.post("/blogs", function (req, res) { ... })` is defining a route for the POST request
+to "/blogs". When this route is accessed, it executes the callback function. */
 app.post("/blogs", function (req, res) {
-  // Obtenemos los datos del nuevo blog desde el body del request
   var title = req.body.title;
   var author = req.body.author;
   var date = req.body.date;
   var content = req.body.content;
 
-  console.log(req.body, "data", title, author, date, content);
-
-  // Insertamos el nuevo blog en la base de datos
   connection.query(
     "INSERT INTO articles (title, author, date, content) VALUES (?, ?, ?, ?)",
     [title, author, date, content],
     function (error, result) {
       if (error) {
-        res.send(error); // Si hay un error en la consulta, lo enviamos como respuesta
+        res.send(error);
       } else {
-        res.send("Blog creado correctamente"); // Si la consulta fue exitosa, enviamos un mensaje de exito como respuesta
+        res.send("Blog creado correctamente");
       }
     }
   );
 });
 
-// Endpoint para actualizar un blog
+/* The code `app.put("/blogs/:id", function (req, res) { ... })` is defining a route for the PUT
+request to "/blogs/:id". This route expects a parameter `id` in the URL. When this route is
+accessed, it executes the callback function. */
 app.put("/blogs/:id", function (req, res) {
-  // Obtenemos el id del blog a actualizar de la URL
   var id = req.params.id;
 
-  // Obtenemos los datos actualizados del blog desde el body del request
   var title = req.body.title;
   var author = req.body.author;
   var date = req.body.date;
@@ -96,47 +92,42 @@ app.put("/blogs/:id", function (req, res) {
 
   console.log(req.body, "data", title, author, date, content);
 
-  // Actualizamos el blog en la base de datos
   connection.query(
     `UPDATE articles SET title = ?, author = ?, date = ?, content = ? WHERE id = ?`,
     [title, author, date, content, id],
     function (error, result) {
       if (error) {
-        res.send(error); // Si hay un error en la consulta, lo enviamos como respuesta
+        res.send(error);
       } else {
-        res.send("Blog actualizado correctamente"); // Si la consulta fue exitosa, enviamos un mensaje de exito como respuesta
+        res.send("Blog actualizado correctamente");
       }
     }
   );
 });
 
-// Endpoint para eliminar un blog
+/* The code `app.delete("/blogs/:id", function (req, res) { ... })` is defining a route for the DELETE
+request to "/blogs/:id". This route expects a parameter `id` in the URL. When this route is
+accessed, it executes the callback function. */
 app.delete("/blogs/:id", function (req, res) {
-  // Obtenemos el id del blog a eliminar de la URL
   var id = req.params.id;
 
-  // Eliminamos el blog de la base de datos
   connection.query(
     "DELETE FROM articles WHERE id = ?",
     [id],
     function (error, result) {
       if (error) {
-        res.send(error); // Si hay un error en la consulta, lo enviamos como respuesta
+        res.send(error);
       } else {
-        res.send("Blog eliminado correctamente"); // Si la consulta fue exitosa, enviamos un mensaje de exito como respuesta
+        res.send("Blog eliminado correctamente");
       }
     }
   );
 });
 
-// Iniciamos el servidor en el puerto 3000
+/* The code `app.listen(3008, function () {
+  console.log("Servidor iniciado en el puerto 3008");
+});` is starting the server and listening for incoming requests on port 3008. Once the server is
+started, it will log the message "Servidor iniciado en el puerto 3008" to the console. */
 app.listen(3008, function () {
-  console.log(
-    "Servidor iniciado en el puerto 3008",
-    DB_HOST,
-    DB_USER,
-    DB_PASSWORD,
-    DB_NAME,
-    DB_PORT
-  );
+  console.log("Servidor iniciado en el puerto 3008");
 });
